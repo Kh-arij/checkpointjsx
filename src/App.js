86a036f10df12b78320db96b5541a0d5700a2d69
Arij <khaldi.arij9@gmail.com>
2021-04-16 11:15:00 +0100

    cpJsx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import   imgone from './images/b.jpg'
+import   video from './video/V.mp4'
 function App() {
+
+  const  style_img ={
+    display:'flex',
+    flexDirection:'row',
+    justifyContent:'space-around'
+  }
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+              <div>
+                        
+
+
+
+              <h1>        Arij Khaldi           </h1>
+
+
+
+
+              <video width="320" height="240" src={video}  type="video/mp4" controls/>
+
+
+
+
+            
+                <div  style={style_img}>
+
+                <img   src="imges/a.jpg"       style={{width:'220px'}}  />
+
+<img   src={imgone}     style={{width:'220px'}} />
+                </div>
+        
+              </div>
+           
+                         
+
+                        
+
+
     </div>
+    
   );
 }
 
